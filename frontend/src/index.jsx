6d2b36ae1a0f904c60e8ee06129bcb87fada480c
Aleksandra <aleksandra.korza@mail.ru>
2026-09-22 10:40:49 +0200

    fix: update init import path in index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,9 @@
-import ReactDOM from 'react-dom/client';
-
-import init from './init.js';
+// ВАЖНО: импортируем без расширения .js / .jsx
+import init from './init'; 
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
-
   const vdom = await init();
-
   root.render(vdom);
 };
 
